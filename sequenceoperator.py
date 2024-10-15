9f79bcac80f207e5b2b3a4721b9603b0b9452804
Harsha Reddy <harshanaayini@gmail.com>
2024-10-14 22:09:19 -0400

    strings slices

---
+++ sequenceoperator.py
@@ -0,0 +1,6 @@
+age = 24
+print("my age is {0} years" .format(age))
+
+for i in range(1,13):
+  #print("no. {0} squre {1} " .format(i, i**2))
+  print("no. {0:2} squre {1:4} " .format(i, i**2))
--- variables.py
+++ variables.py
@@ -1,2 +1,21 @@
 # variables and Types
+# slices
+parrot = "Norwegian blue"
+
+print(parrot[0:6])
+print(parrot[0:5]) # upto but not including last value
+print(parrot[:5]) # it same as above
+print(parrot[10:14])
+print(parrot[10:])
+
+print(parrot[:]) # it print everything bigging to end
+print(parrot[:6] + parrot[6:])
+
+print(parrot[-4:12]) # bl
+print(parrot[-4:-12]) # bl
+
+# step
+
+print(parrot[0:6:2])
+
 
